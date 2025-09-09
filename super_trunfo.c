a71a347b7a2516866d6c1e6fe4bc037f3587573b
matheusfar <matheusf.ar@hotmail.com>
2025-09-09 23:31:10 +0000

    Refeito declaração de variaveis

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main(){
+
+
+    char estado1 , estado2; // recebe uma letra referente ao estado
+
+    char codigo_carta1[50] , codigo_carta2[50]; // recebe o codigo da carta
+
+    char cidade1[50] , cidade2[50]; // recebe o nome da cidade
+
+    int  populacao1, populacao2; // recebe a população da cidade
+
+    int ponto_turistico1, ponto_turistico2; // recebe o numero de pontos turisticos da cidade
+
+    float area1, area2; //recebe o total da area da cidade
+
+    float pib1, pib2; // recebe o PIB da cidade.
+
+
+
+    return 0;
+}
